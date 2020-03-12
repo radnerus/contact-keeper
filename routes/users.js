2fd754f62832a387d616fc019f7dbe96475537c6
radnerus <surendarandyou@gmail.com>
2020-03-12 08:42:48 +0530

    from mac

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const User = require('../models/User');
 
 const router = express.Router();
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post('/', (req, res) => res.send('Registers an user'));
+router.post('/', (req, res) => res.send(req.body));
 
 module.exports = router;
